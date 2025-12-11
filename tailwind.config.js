1ab45dc4e1003f6314f4d2c957ff8754be8fdb1e
sarahInaoui <agility.inaoui@gmail.com>
2025-12-11 16:43:28 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,8 @@
-// tailwind.config.js
-export default {
-  content: ["./*.{html,js}"], // scan all .html and .js files in root
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          DEFAULT: '#1e40af',
-          light: '#3b82f6',
-          dark: '#1e3a8a',
-        },
-      },
-      spacing: {
-        128: '32rem', // generates class `p-128`, `m-128`, etc.
-      },
-      fontFamily: {
-        display: ['Oswald', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
-  /* This way, you can use classes like bg-brand, bg-brand-dark, p-128, or font-display. */
-  plugins: [
-    function ({ addUtilities }) {
-      const newUtilities = {
-        '.rotate-y-180': {
-          transform: 'rotateY(180deg)',
-        },
-        '.perspective-1000': {
-          perspective: '1000px',
-        },
-      }
-      addUtilities(newUtilities, ['responsive', 'hover'])
-    },
-  ],
-  /* Now you can use rotate-y-180 or hover:perspective-1000. */
-}
+  plugins: [],
+};
